095fae21ce8fae5d9d4f64aa134edf7134ab59c6
Kevin Huck <khuck@cs.uoregon.edu>
2015-11-10 18:26:10 -0800

    Fixing the path to the generated modulefiles.

--- babbage/env.sh
+++ babbage/env.sh
@@ -1,3 +1,3 @@
 
-TAU_MODULEPATH=/project/projectdirs/xpress/tau2-hpx/modulefiles
-HPX_MODULEPATH=$BASE_PATH/modulefiles/babbage/hpx
+TAU_MODULEPATH=/project/projectdirs/xpress/tau2-hpx-babbage/modulefiles
+HPX_MODULEPATH=$BASE_PATH/modulefiles
