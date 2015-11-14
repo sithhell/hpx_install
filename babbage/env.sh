8381d4fce7d97fed9d9202d5a211fb5c53c1f1aa
Kevin Huck <khuck@cs.uoregon.edu>
2015-11-13 17:17:17 -0800

    Updating paths to TAU modules to work in training directory.

--- babbage/env.sh
+++ babbage/env.sh
@@ -1,3 +1,3 @@
 
-TAU_MODULEPATH=/project/projectdirs/xpress/tau2-hpx-babbage/modulefiles
+TAU_MODULEPATH=$BASE_PATH/../tau2-hpx-babbage/modulefiles
 HPX_MODULEPATH=$BASE_PATH/modulefiles
--- edison/env.sh
+++ edison/env.sh
@@ -1,3 +1,3 @@
 
-TAU_MODULEPATH=/project/projectdirs/xpress/tau2-hpx-edison/modulefiles
+TAU_MODULEPATH=$BASE_PATH/../tau2-hpx-edison/modulefiles
 HPX_MODULEPATH=$BASE_PATH/modulefiles
